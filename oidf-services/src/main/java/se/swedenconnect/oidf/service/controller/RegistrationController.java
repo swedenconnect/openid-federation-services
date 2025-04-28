c2f884a62b8b42658648d6ef66619a813b28e946
Felix Hellman <felix@helagon.se>
2025-04-28 12:56:07 +0200

    fix: Add interop registration endpoint

---
+++ oidf-services/src/main/java/se/swedenconnect/oidf/service/controller/RegistrationController.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2024-2025 Sweden Connect
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package se.swedenconnect.oidf.service.controller;
+
+import com.nimbusds.openid.connect.sdk.federation.entities.EntityID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import se.swedenconnect.oidf.common.entity.entity.integration.registry.records.EntityRecord;
+
+import java.util.List;
+
+/**
+ * Temp controller.
+ *
+ * @author Felix Hellman
+ */
+@RestController
+@Slf4j
+public class RegistrationController {
+
+  public RegistrationController(final RegistrationSource registrationSource) {
+    this.registrationSource = registrationSource;
+  }
+
+  private final RegistrationSource registrationSource;
+
+  /**
+   * @param request Make the documentation happy
+   */
+  @PostMapping("/register")
+  public void register(@RequestBody final RegistrationRequest request) {
+    log.info("Recieved request {}", request);
+    this.registrationSource.addEntity(new EntityRecord(
+        new EntityID("https://dev.swedenconnect.se/interop/im"),
+        request.getSubject(),
+        null,
+        request.getJwks(),
+        null,
+        null,
+        List.of(),
+        List.of()
+    ));
+  }
+}
---
+++ oidf-services/src/main/java/se/swedenconnect/oidf/service/controller/RegistrationSource.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2024-2025 Sweden Connect
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package se.swedenconnect.oidf.service.controller;
+
+import com.nimbusds.openid.connect.sdk.federation.entities.EntityID;
+import se.swedenconnect.oidf.common.entity.entity.integration.RecordSource;
+import se.swedenconnect.oidf.common.entity.entity.integration.properties.ResolverProperties;
+import se.swedenconnect.oidf.common.entity.entity.integration.properties.TrustAnchorProperties;
+import se.swedenconnect.oidf.common.entity.entity.integration.properties.TrustMarkIssuerProperties;
+import se.swedenconnect.oidf.common.entity.entity.integration.registry.TrustMarkId;
+import se.swedenconnect.oidf.common.entity.entity.integration.registry.records.EntityRecord;
+import se.swedenconnect.oidf.common.entity.entity.integration.registry.records.TrustMarkSubjectRecord;
+import se.swedenconnect.oidf.common.entity.tree.NodeKey;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class RegistrationSource implements RecordSource {
+
+  private final List<EntityRecord> registerdEntities;
+
+  public RegistrationSource() {
+    this.registerdEntities = new ArrayList<>();
+  }
+
+  public void addEntity(final EntityRecord record) {
+    this.registerdEntities.add(record);
+  }
+
+
+  @Override
+  public List<TrustMarkIssuerProperties> getTrustMarkIssuerProperties() {
+    return List.of();
+  }
+
+  @Override
+  public List<TrustAnchorProperties> getTrustAnchorProperties() {
+    return List.of();
+  }
+
+  @Override
+  public List<ResolverProperties> getResolverProperties() {
+    return List.of();
+  }
+
+  @Override
+  public Optional<EntityRecord> getEntity(final NodeKey key) {
+    return this.registerdEntities.stream()
+        .filter(key::matches)
+        .findAny();
+  }
+
+  @Override
+  public List<EntityRecord> getAllEntities() {
+    return this.registerdEntities;
+  }
+
+  @Override
+  public List<EntityRecord> findSubordinates(final String issuer) {
+    return List.of();
+  }
+
+  @Override
+  public List<TrustMarkSubjectRecord> getTrustMarkSubjects(final EntityID issuer, final TrustMarkId id) {
+    return List.of();
+  }
+
+  @Override
+  public Optional<TrustMarkSubjectRecord> getTrustMarkSubject(final EntityID issuer, final TrustMarkId id, final EntityID subject) {
+    return Optional.empty();
+  }
+
+  @Override
+  public int priority() {
+    return 2;
+  }
+}
