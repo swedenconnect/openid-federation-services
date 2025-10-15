3929202bc05fb71b378b48d5376116c9e89cca02
PF Plars <plarsa@users.noreply.github.com>
2025-10-15 08:21:11 +0200

    Add JWKSet (de)serialization support in StateHashFactory (#138)
    * Add JWKSet (de)serialization support in StateHashFactory
    * Add JWK handling and preserve JSON order in serializers, so that the calculated checksum will be the same

--- oidf-services/src/main/java/se/swedenconnect/oidf/service/state/StateHashFactory.java
+++ oidf-services/src/main/java/se/swedenconnect/oidf/service/state/StateHashFactory.java
@@ -16,13 +16,29 @@
  */
 package se.swedenconnect.oidf.service.state;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.nimbusds.jose.jwk.JWKSet;
 import org.bouncycastle.util.encoders.Hex;
 import se.swedenconnect.oidf.common.entity.entity.integration.registry.records.CompositeRecord;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
 
 /**
  * Class responsible for hashing internal state.
@@ -34,6 +50,12 @@ public class StateHashFactory {
 
   static {
     MAPPER.registerModule(new JavaTimeModule());
+
+    final SimpleModule module = new SimpleModule();
+    module.addSerializer(JWKSet.class, new JWKSetSerializer());
+    module.addDeserializer(JWKSet.class, new JWKSetDeserializer());
+    MAPPER.registerModule(module);
+
   }
 
   /**
@@ -46,5 +68,61 @@ public class StateHashFactory {
     final MessageDigest digest = MessageDigest.getInstance("SHA-256");
     final byte[] hash = digest.digest(json.getBytes(StandardCharsets.UTF_8));
     return new String(Hex.encode(hash));
+  }
+
+  /**
+   * A deserializer for {@link JWKSet} instances used to convert JSON representations into {@link JWKSet} objects.
+   */
+  public static class JWKSetDeserializer extends JsonDeserializer<JWKSet> {
+    @Override
+    public JWKSet deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException {
+      final Map<String, Object> jwkMap = p.readValueAs(new TypeReference<>() {});
+      try {
+        return JWKSet.parse(jwkMap);
+      }
+      catch (final Exception e) {
+        throw new IOException("Failed to parse JWKSet", e);
+      }
+    }
+  }
+
+  /**
+   * A custom serializer for {@link JWKSet} objects. This serializer ensures that the JSON representation
+   * of the {@link JWKSet} maintains a predictable order of keys by recursively sorting the keys alphabetically.
+   * This is achieved by processing the {@link JWKSet} into a {@link TreeMap} structure where applicable.
+   */
+  public static class JWKSetSerializer extends JsonSerializer<JWKSet> {
+    private Object preserveOrderRecursive(final Object obj) {
+      if (obj instanceof Map<?, ?> map) {
+        final TreeMap<String, Object> treeMap = new TreeMap<>();
+        for (Map.Entry<?, ?> entry : map.entrySet()) {
+          final String key = entry.getKey().toString();
+          final Object value = this.preserveOrderRecursive(entry.getValue()); // rekursivt
+          treeMap.put(key, value);
+        }
+        return treeMap;
+      }else if (obj instanceof List<?> list) {
+        final List<Object> newList = new ArrayList<>();
+        for (Object item : list) {
+          newList.add(this.preserveOrderRecursive(item));
+        }
+        return newList;
+      }
+      else {
+        return obj;
+      }
+    }
+
+    @Override
+    public void serialize(final JWKSet value,
+        final JsonGenerator gen,
+        final SerializerProvider serializers) throws IOException {
+
+      if (value == null) {
+        gen.writeNull();
+        return;
+      }
+      gen.writeObject(this.preserveOrderRecursive(value.toJSONObject()));
+    }
   }
 }
